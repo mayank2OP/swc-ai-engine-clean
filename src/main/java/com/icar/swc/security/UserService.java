474fc99e4a476fcba96647aa1103413f22c95a7d
mayank2OP <mayankrawat460@gmail.com>
2026-01-31 14:54:30 +0530

    Create UserService.java

---
+++ src/main/java/com/icar/swc/security/UserService.java
@@ -0,0 +1,42 @@
+package com.icar.swc.security;
+
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.icar.swc.entity.User;
+import com.icar.swc.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // ======================================================
+    // LOAD USER FOR SPRING SECURITY
+    // ======================================================
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword() == null ? "" : user.getPassword(),
+                Collections.singletonList(
+                        new SimpleGrantedAuthority("ROLE_" + user.getRole())
+                )
+        );
+    }
+}
