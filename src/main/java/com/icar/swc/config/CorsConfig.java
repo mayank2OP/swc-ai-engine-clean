a3b26dde1f5ae76b8c6da827a47b10a3c40fc68e
mayank2OP <mayankrawat460@gmail.com>
2026-01-31 20:02:44 +0530

    Update CorsConfig.java

--- src/main/java/com/icar/swc/config/CorsConfig.java
+++ src/main/java/com/icar/swc/config/CorsConfig.java
@@ -1,1 +1,29 @@
+package com.icar.swc.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://swc-ai-engine-clean.vercel.app",
+                                "https://swc-ai-engine-clean-git-7a8a25-mayank-rawats-projects-d0f398be.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
